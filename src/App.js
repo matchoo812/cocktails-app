87dc9a5ab495cf0183922cd0116a19ab88cfa2b2
Matt Ballard <mattjballard81@gmail.com>
2022-09-13 10:25:55 +0900

    initial setup of routes and context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import pages
+import SharedLayout from "./pages/SharedLayout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
+// import components
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='/cocktail/:id' element={<SingleCocktail />} />
+          <Route path='/*' element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

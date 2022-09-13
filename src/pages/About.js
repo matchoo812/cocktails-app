19b0f14e27fc438a03ed416f430b86d0659942b1
Matt Ballard <mattjballard81@gmail.com>
2022-09-13 10:56:31 +0900

    add 404 and About pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,18 @@
-import React from "react";
-
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni officiis
+        culpa, doloribus quas ad minus architecto accusantium animi ipsa rem
+        voluptate sapiente vitae mollitia libero consequatur, soluta odit asperiores
+        accusamus eos optio! Eligendi distinctio odio odit velit sequi, culpa alias
+        tenetur illum voluptatum dolor hic placeat qui, quisquam voluptas illo
+        perspiciatis magnam quam laboriosam dolorum, inventore ducimus consectetur.
+        Aperiam, vitae?
+      </p>
+    </section>
+  );
 };
 
 export default About;
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,16 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <section className='section error-page-section'>
+      <div className='error-container'>
+        <h1>Oops! You've gone to the wrong place. No cocktails here!</h1>
+        <Link to='/' className='btn btn-primary'>
+          Go Back Home
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Error;
